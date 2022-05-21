94247817f01ad92c403c2e389698bb2b641804c0
maheshkrishnachalla <maheshkrishnachalla@gmail.com>
2022-05-21 20:08:24 +0530

    new programs

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ public class Main {
         System.out.println("your roll no is "+rollNo);*/
         int a =230_000_000;
         System.out.println(a);
-
         String name = scanner.nextLine();
         System.out.println("name "+name);
         float marks = scanner.nextFloat();
